89cf5cf7b7307d87faf34f5cbf873cae4f0a2a7b
declanCross <declancross38@outlook.com>
2023-08-11 15:25:01 +1000

    clean up template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Jammmin</h1>
+			</header>
+			<main className="App-main"></main>
+			<footer className="App-footer"></footer>
+		</div>
+	);
 }
 
 export default App;
