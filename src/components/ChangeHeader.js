250f45b726bd2cfdaabdd8e2a7ff342c5c6dfba6
declanCross <declancross38@outlook.com>
2023-08-15 16:57:20 +1000

    implement change header on rerender

--- src/components/ChangeHeader.js
+++ src/components/ChangeHeader.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 export default function ChangeHeader() {
+	const jammminLyrics = [
+		"We're jammmin",
+		"I hope you like jammmin too",
+		"Jammmin 'till the jam is through",
+		"We're jammmin in the name of the Lord",
+		"We're jammmin right straight from yard",
+		"Jam down hope you're jammmin too",
+		"We're jammmin, we're jammmin, we're jammmin, we're jammmin",
+	];
+	const jammminLyric =
+		jammminLyrics[Math.floor(Math.random() * jammminLyrics.length)];
 	return (
 		<>
-			<div>ChangeHeader</div>
+			<div>"{jammminLyric}"</div>
 		</>
 	);
 }
