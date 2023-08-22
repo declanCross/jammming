f201b28715748f4e7fb475463ed4c3ece3f0d75c
declanCross <declan_cross@outlook.com>
2023-08-22 18:55:41 +1000

    improve search functionality to include albums and artists

--- src/components/SpotifySearch.js
+++ src/components/SpotifySearch.js
@@ -12,7 +12,7 @@ const SpotifySearch = ({ token, results, setResults }) => {
 	const handleSearch = async (event) => {
 		event.preventDefault();
 		const response = await fetch(
-			`https://api.spotify.com/v1/search?q=${query}&type=track`,
+			`https://api.spotify.com/v1/search?q=${query}&type=track,album,artist`,
 			{
 				headers: {
 					Authorization: `Bearer ${token}`,
